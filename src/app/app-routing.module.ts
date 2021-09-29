4a25e250e59f4eca79ad2c85ea121ddff0d1bf1f
sebastian-migacz <vkensaj@gmail.com>
2021-09-29 15:34:42 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DeviceComponent } from './device/device.component';
+
+
+const routes: Routes = [{path: 'devices', component: DeviceComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
